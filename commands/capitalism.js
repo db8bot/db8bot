41d84122c6340a81d7db8c9c716aeae7b741e668
AirFusion <jfang.cv.ca.us@gmail.com>
2020-04-12 01:17:34 -0700

    update fs write problem to check dup quotes

--- commands/capitalism.js
+++ commands/capitalism.js
@@ -28,7 +28,7 @@ exports.run = function (client, message) {
     client.logger.log('info', `capitalism command used by ${message.author.tag} ID: ${message.author.id} Time: ${Date()} Guild: ${message.guild}`)
     quotes[quotes.length - 1].lastQuote = quotes[num].quote
     quotes[quotes.length - 1].lastNumber = num
-    fs.writeFile('../quotesCap.json', JSON.stringify(quotes, null, 2), function (err) {
+    fs.writeFile('./quotesCap.json', JSON.stringify(quotes, null, 2), function (err) {
         if (err) return console.error(err);
         // message.channel.send(`Prefix Successfully Changed to ${config.prefix}.`)
     });
--- commands/communism.js
+++ commands/communism.js
@@ -28,7 +28,7 @@ exports.run = function (client, message) {
     client.logger.log('info', `communism command used by ${message.author.tag} ID: ${message.author.id} Time: ${Date()} Guild: ${message.guild}`)
     quotes[quotes.length - 1].lastQuote = quotes[num].quote
     quotes[quotes.length - 1].lastNumber = num
-    fs.writeFile('../quotes.json', JSON.stringify(quotes, null, 2), function (err) {
+    fs.writeFile('./quotes.json', JSON.stringify(quotes, null, 2), function (err) {
         if (err) return console.error(err);
         // message.channel.send(`Prefix Successfully Changed to ${config.prefix}.`)
     });
