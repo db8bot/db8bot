312a4d25ea43b5b640c006d67731cb4ba059dbcf
AirFusion <jfang.cv.ca.us@gmail.com>
2020-05-14 01:11:33 -0700

    add debate help to prevent ppl not knowing how to use stuff https://huss.is-inside.me/hEWEHTKG.png

---
+++ commands/debatehelp.js
@@ -0,0 +1,21 @@
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+exports.run = function (client, message) {
+
+    const Discord = require('discord.js');
+    const config = client.config;
+    var guild = message.guild;
+    var select = getRandomIntInclusive(1, 3);
+    const embed = new Discord.MessageEmbed()
+        .setColor(select === 1 ? "#ccff00" : select === 2 ? "#0072bb" : select === 3 ? "#ff4f00" : "#ccff00")
+        .setTitle("DB8Bot Debate Function Help\n")
+        .addField(`Start a round`, `Use the startround command`)
+        .addField(`Move a round forward`, `Set the current speech using the setspeech command. For available use the speeches command`)
+        .addField(`Check a round's status`, `Use the roundstatus command. Rounds are identified by the name assigned at the start of the round.`)
+        .addField(`Flip a coin for PF`, `Use the flip command`)
+    message.channel.send({ embed: embed })
+    client.logger.log('info', `debatehelp command used by ${message.author.tag} ID: ${message.author.id} Time: ${Date()} Guild: ${message.guild}`)
+}
