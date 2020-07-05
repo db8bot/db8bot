8c948acba560c61422281d7a9f3709760f32aaab
AirFusion <jfang.cv.ca.us@gmail.com>
2020-07-04 18:54:44 -0700

    next version deploy ready!

--- commands/commands.js
+++ commands/commands.js
@@ -9,11 +9,11 @@ exports.run = function (client, message) {
         .setFooter(`${config.name} Commands`)
         .setTimestamp()
         .setTitle(`Please use the prefix "${config.prefix}" in front of all commands!`)
-        .addField(`:tools: **General**`, `commands, help, embed, feedback, invite, ping, say, serverinv, dm, optout, opts`, true)
+        .addField(`:tools: **General**`, `commands, help, embed, feedback, invite, ping, say, serverinv, dm, optout, opts, clean`, true)
         .addField(`:hammer: **Moderation**`, `lockdown (lockdown unlock), mute & unmute(mute role required), purge, setup`, true)
         .addField(`:information_source: **Information**`, `botinfo, serverinfo, userinfo`, true)
         .addField(`:trophy: **Debate**`, `get, **startround: setspeech, endround, roundstatus, flip**, judgeinfo, speeches`, true)
-        .addField(`:partying_face: **Fun**`, `communism, capitalism, trump, baudrillard, bataille`, true)
+        .addField(`:partying_face: **Fun**`, `communism, capitalism, trump, baudrillard, bataille, agamben, foucault`, true)
 
         message.channel.send({ embed: embedNew })
         client.logger.log('info', `commands command used by ${message.author.tag} ID: ${message.author.id} Time: ${Date()} Guild: ${guild}`)
