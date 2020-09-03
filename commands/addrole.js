62d40208d1e95c2ec7ba491cf85d9a9a8982cd2b
AirFusion <jfang.cv.ca.us@gmail.com>
2020-09-03 11:08:32 -0700

    push to avoid conflicts

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,7 +2,7 @@ exports.run = function (client, message) {
     if (message.guild.id != "685646226942984206") return;
     else {
         const member = message.guild.member(message.mentions.users.first());
-        const role = message.guild.roles.cache.find(role => role.name === "testinglol");
+        const role = message.guild.roles.cache.find(role => role.name === "DJ");
         member.roles.add(role);
     }
     // .then((GuildMember) => {
