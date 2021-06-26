a46bf67b09769af4e17f5b0d05e2487bfe3e1398
AirFusion <jfang.cv.ca.us@gmail.com>
2021-06-25 23:27:00 -0700

    begin fix

--- mediaProfilesBlockedPageReqRegex.js
+++ mediaProfilesBlockedPageReqRegex.js
@@ -5,7 +5,7 @@ module.exports = {
   'chicagotribune.com': /.+:\/\/.+\.tribdss\.com\//,
   'economist.com': /(.+\.tinypass\.com\/.+|economist\.com\/engassets\/_next\/static\/chunks\/framework.+\.js)/,
   'editorialedomani.it': /(js\.pelcro\.com\/.+|editorialedomani.it\/pelcro\.js)/,
-  'foreignpolicy.com': /.+\.tinypass\.com\/.+/,
+  'foreignpolicy.com': /(cdn\.cxense\.com\/|\.tinypass\.com\/)/,
   'fortune.com': /.+\.tinypass\.com\/.+/,
   'haaretz.co.il': /haaretz\.co\.il\/htz\/js\/inter\.js/,
   'haaretz.com': /haaretz\.com\/hdc\/web\/js\/minified\/header-scripts-int.js.+/,
