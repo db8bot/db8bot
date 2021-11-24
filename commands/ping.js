d1fd409a7a3de3a3bab0dc9f0d91a455d20ecd17
AirFusion45 <jfang.cv.ca.us@gmail.com>
2021-11-23 17:03:13 -0800

    GA custom fields!

--- commands/ping.js
+++ commands/ping.js
@@ -7,11 +7,14 @@ module.exports = {
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
         interaction.client.logger.log('info', `ping command used by ${interaction.user.username} Time: ${Date()} Guild: ${interaction.guild.name}`)
+        console.log(Buffer.from(interaction.user.username).toString('base64'))
+        interaction.client.telemetry.set('cd[1]', interaction.user.username)
         interaction.client.telemetry.pageview({
             v: interaction.client.pkg.version.replace(/\./g, '').trim(),
             cid: interaction.user.username, // old client id
             // uid: interaction.user.username, // trial 1
-            uid: 'e6dcc4ca-dd85-4a3f-8869-2e66505ab344', // trial 1
+            uid: Buffer.from(interaction.user.username).toString('base64'), // trial 2
+            // uid: 'dGVzdGluZyB0ZXN0aW5nPQ==', // trial 3
             dp: `/${path.basename(__filename, '.js')}`,
             dt: path.basename(__filename, '.js'),
             // dr: `https://discord.com/server/${interaction.guild.name}`
