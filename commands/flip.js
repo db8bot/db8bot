db24436f09e6e7ffa4691632490f94fe2a668000
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2021-12-13 11:47:35 +0530

    updated coin Math.random and added embed

--- commands/flip.js
+++ commands/flip.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const Discord = require('discord.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,6 +7,15 @@ module.exports = {
         .setDescription('Flip a Coin - Useful for PF'),
     async execute(interaction) {
         require('../modules/telemetry').telemetry(__filename, interaction)
-        interaction.reply((Math.floor(Math.random() * 2) === 0) ? 'heads' : 'tails')
+         let coin = [
+            'tails',
+            'heads',
+        ]
+        let index = (Math.floor(Math.random() * Math.floor(coin.length)))
+        const embed = new Discord.MessageEmbed()
+        .setColor('00FFFF')
+        .setTitle(coin[index] )
+        interaction.reply({ embeds: [embed] })
+    
     }
 }
