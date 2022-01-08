2b2702147b7adf51a362352dbfdf1d76dfdd44d5
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-01-08 01:43:05 -0800

    gonzaga patches

--- routes/mailIn.js
+++ routes/mailIn.js
@@ -11,27 +11,39 @@ router.post('/', upload.any(), async (req, resApp) => {
     const client = req.app.get('client')
     // check if sender is blasts@www.tabroom.com OR live@tabroom.com - correct email address needs to be confirmed during testing so we will use both for now
     // check title containes "round"
-    var sender = req.body.from
+    // eslint-disable-next-line no-control-regex
+    var sender = req.body.from.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gmi)[0]
     var title = req.body.subject
-    if ((sender === 'blasts@www.tabroom.com' || sender === 'live@tabroom.com') && (title.toLowerCase().includes('round'))) {
+    console.log(sender)
+    console.log(title.toLowerCase())
+    if ((sender === 'blasts@www.tabroom.com' || sender === 'live@tabroom.com' || sender === 'yfang@thecollegepreparatoryschool.org') && (title.toLowerCase().includes('round'))) {
         var dbClient = await database.connect()
         var collection = dbClient.db('db8bot').collection('tabroomLiveUpdates')
 
         var receiveDate = new Date(req.body.headers.match(/Date: .+?(?=\n)/gmi)[0].trim().replace('Date: ', ''))
-        var msg = req.body.text.split('\n')
-        var searchString = msg[0].split('vs.')
+        var msg = req.body.text
+        console.log(msg)
+        var competition = msg.substring(0, msg.toLowerCase().indexOf('judging:')).replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
+        console.log(competition)
+        var searchString = competition.split('vs.')
         searchString.forEach((x, index) => {
             // this is only for policy as of now. pf prob says pro/con. also flip for sides may come before [0]
-            if (x.substring(0, 3) === 'Aff') {
+            if (x.trim().substring(0, 3) === 'Aff') {
                 searchString[index] = x.replace('Aff', '').trim()
-            } else if (x.substring(0, 3) === 'Neg') {
+            } else if (x.trim().substring(0, 3) === 'Neg') {
                 searchString[index] = x.replace('Neg', '').trim()
             }
         })
-        var judging = msg[2]
-        var start = msg[3].toLowerCase().replace('start', '').replace('p', 'pm').replace('a', 'am').trim()
-        var room = msg[4].toLowerCase().replace('room: ', '').trim()
-        var extraInfo = msg.slice(5).join('\n')
+        console.log(searchString)
+        var judging = msg.match(/(?<=judging:)[\s\S]*(?=start)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
+        var start = msg.match(/(?<=start)[\s\S]*(?=room:)/gmi)[0].toLowerCase().replace(/\n/gmi, '').replace(/\r/gmi, ' ').replace('p', 'pm').replace('a', 'am').trim()
+        var room
+        if (msg.toLowerCase().includes('message:')) {
+            room = msg.match(/(?<=room:)[\s\S]*(?=message:)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
+        } else {
+            room = msg.match(/(?<=room:)[\s\S]*(?=\n)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
+        }
+        var extraInfo = msg.substring((msg.indexOf(room) + room.length))
 
         var searchStr0Res = await collection.find({
             trackedTeamCode: searchString[0],
@@ -56,11 +68,11 @@ router.post('/', upload.any(), async (req, resApp) => {
         if (searchStr0Res.length > 0) {
             // find server, find channel, send message
             console.log(`${searchString[0]} found in db`)
-            notifyServer(searchStr0Res[0], { msg: msg, title: title, judging: judging, start: start, room: room, extraInfo: extraInfo })
+            notifyServer(searchStr0Res[0], { msg: msg, competition: competition, title: title, judging: judging, start: start, room: room, extraInfo: extraInfo })
         }
         if (searchStr1Res.length > 0) {
             console.log(`${searchString[1]} found in db`)
-            notifyServer(searchStr1Res)
+            notifyServer(searchStr1Res[0], { msg: msg, competition: competition, title: title, judging: judging, start: start, room: room, extraInfo: extraInfo })
         }
         async function notifyServer(searchStrRes, content) {
             var notifyArr = searchStrRes.notify
@@ -73,7 +85,7 @@ router.post('/', upload.any(), async (req, resApp) => {
                         const embed = new Discord.MessageEmbed()
                             .setTitle(`${content.title} Pairings`)
                             .setColor('#daeaf1')
-                            .addField('Competition', content.msg[0])
+                            .addField('Competition', content.competition)
                             .addField('Judging', content.judging)
                             .addField('Start Time', content.start)
                             .addField('Room', content.room)
