248084bff1e152c7ec9ee6ba538cbd6d34d1bbbe
Pranshu Patel <70943732+pranshu05@users.noreply.github.com>
2022-01-15 19:15:05 +0530

    Updated to process.env

--- commands/unmute.js
+++ commands/unmute.js
@@ -14,7 +14,7 @@ module.exports = {
         require('../modules/telemetry').telemetry(__filename, interaction)
         var user = interaction.options.getUser('user')
         const muteRole = interaction.guild.roles.cache.find(val => val.name === 'Mute')
-         if (interaction.guild.members.cache.get(interaction.user.id).permissions.has(Discord.Permissions.FLAGS.MANAGE_MESSAGES) || interaction.guild.members.cache.get(interaction.user.id).permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR) || interaction.user.id === config.OWNER){
+         if (interaction.guild.members.cache.get(interaction.user.id).permissions.has(Discord.Permissions.FLAGS.MANAGE_MESSAGES) || interaction.guild.members.cache.get(interaction.user.id).permissions.has(Discord.Permissions.FLAGS.ADMINISTRATOR) || interaction.user.id === process.env.OWNER){
         if (!muteRole) return interaction.reply('Mute Role required')
         if (user === interaction.user) return interaction.reply('You cannot mute yourself')
         interaction.guild.members.fetch(interaction.client.user.id).then(member => {
