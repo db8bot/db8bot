dbba66b95c8f4d50f23c1a0746661b2eb0bad835
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-01-22 17:03:45 -0800

    dev: cleaner way to rewrite- policy msg msg processing done - need pf/ld

--- routes/mailIn.js
+++ routes/mailIn.js
@@ -9,6 +9,79 @@ const Discord = require('discord.js')
 
 router.post('/', upload.any(), async (req, resApp) => {
     const client = req.app.get('client')
+
+    async function notifyServer(searchStrRes, content) {
+        var notifyArr = searchStrRes.notify
+        for (const server of notifyArr) {
+            var sendGuild = await client.guilds.fetch(server.server)
+            if (sendGuild.available) {
+                var guildChannels = await sendGuild.channels.fetch(server.channel)
+                try {
+                    var tagging = server.users.map(user => `<@${user}>`).join(', ') + '\n' + (server.role !== null ? `<@&${server.role}>` : '')
+                    const embed = new Discord.MessageEmbed()
+                        .setTitle(`${content.title} Pairings`)
+                        .setColor('#daeaf1')
+                    if (content.bye) { // if is bye
+                        embed.addField('Competition', content.competition)
+                    } else {
+                        embed.addField('Competition', content.competition)
+                        embed.addField('Judging', content.judging)
+                        embed.addField('Start Time', content.start)
+                        embed.addField('Room', content.room)
+                        embed.addField('Extra Info', content.extraInfo)
+                    }
+                    guildChannels.send({ content: tagging, embeds: [embed] })
+                } catch (err) {
+                    console.error(err)
+                }
+            }
+        }
+    }
+
+    async function messageProcessing(event, msg) {
+        if (event === 'cx') {
+            var competition = msg.substring(0, msg.toLowerCase().indexOf('judging:')).replace(/\n/gmi, ' ').replace(/\r/gmi, ' ').replace(/\t/g, ' ').trim() // tabromm doesnt use \r whereas gmail does. the \r replace is for when its from gmail (testing), \n is for prod
+            console.log(competition)
+
+            var searchString
+            if (competition.toLowerCase().includes('(flip)')) { // if it is a flip - meaning it is prob an elim rd
+                searchString = competition.toLowerCase().replace('(flip)', '').split('vs.')
+                searchString = [searchString[0].trim(), searchString[1].trim()]
+            } else {
+                searchString = competition.toLowerCase().split('vs.')
+                searchString.forEach((x, index) => {
+                    // this is only for policy as of now. pf prob says pro/con. also flip for sides may come before [0]
+                    if (x.trim().substring(0, 3) === 'Aff') {
+                        searchString[index] = x.replace('Aff', '').trim()
+                    } else if (x.trim().substring(0, 3) === 'Neg') {
+                        searchString[index] = x.replace('Neg', '').trim()
+                    }
+                })
+                console.log(searchString)
+            }
+
+            var judging = msg.substring(msg.toLowerCase().indexOf('judging:'), msg.search(/start ([0-9]|[0-9][0-9]):[0-9][0-9] (a|p)/gmi)).replace(/\n/gmi, '  ').replace(/\r/gmi, ' ').trim().split('  ').join(', ') // replace \n with 2 spaces so that we can still .trim() for spaces at the end but also identify where 2 judges are separated.
+            var start = msg.substring(msg.search(/start ([0-9]|[0-9][0-9]):[0-9][0-9] (a|p)/gmi), msg.toLowerCase().indexOf('room: ')).toLowerCase().replace('p', 'pm').replace('a', 'am').trim()
+
+            var room
+            if (msg.toLowerCase().includes('message:')) {
+                room = msg.match(/(?<=room:)[\s\S]*(?=message:)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
+            } else {
+                room = msg.match(/(?<=room:)[\s\S]*(?=\n)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
+            }
+            var extraInfo = msg.substring((msg.indexOf(room) + room.length))
+            return ({
+                competition: competition,
+                searchString: searchString,
+                judging: judging,
+                start: start,
+                room: room,
+                extraInfo: extraInfo
+            })
+        } else if (event === 'pf') {
+        }
+    }
+
     // check if sender is blasts@www.tabroom.com OR live@tabroom.com - correct email address needs to be confirmed during testing so we will use both for now
     // check title containes "round"
     // eslint-disable-next-line no-control-regex
@@ -16,6 +89,7 @@ router.post('/', upload.any(), async (req, resApp) => {
     var title = req.body.subject
     console.log(sender)
     console.log(title.toLowerCase())
+
     if ((sender === 'blasts@www.tabroom.com' || sender === 'yfang@thecollegepreparatoryschool.org') && (!title.toLowerCase().includes('tabroom update') && !title.toLowerCase().includes('message from tab'))) {
         var dbClient = await database.connect()
         var collection = dbClient.db('db8bot').collection('tabroomLiveUpdates')
@@ -25,28 +99,9 @@ router.post('/', upload.any(), async (req, resApp) => {
 
         if (!(req.body.text.split('\n').length < 2) && !(req.body.text.includes('BYE'))) { // if it is not a bye - process the message
             console.log(msg)
-            var competition = msg.substring(0, msg.toLowerCase().indexOf('judging:')).replace(/\n/gmi, ' ').replace(/\r/gmi, ' ').replace(/\t/g, ' ').trim() // tabromm doesnt use \r whereas gmail does. the \r replace is for when its from gmail (testing), \n is for prod
-            console.log(competition)
-            var searchString = competition.toLowerCase().replace('(flip)', '').split('vs.')
-            searchString.forEach((x, index) => {
-                // this is only for policy as of now. pf prob says pro/con. also flip for sides may come before [0]
-                if (x.trim().substring(0, 3) === 'Aff') {
-                    searchString[index] = x.replace('Aff', '').trim()
-                } else if (x.trim().substring(0, 3) === 'Neg') {
-                    searchString[index] = x.replace('Neg', '').trim()
-                }
-            })
-            console.log(searchString)
-            var judging = msg.match(/(?<=judging:)[\s\S]*(?=start)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
-            var start = msg.match(/(?<=start)[\s\S]*(?=room:)/gmi)[0].toLowerCase().replace(/\n/gmi, '').replace(/\r/gmi, ' ').replace('p', 'pm').replace('a', 'am').trim()
-            var room
-            if (msg.toLowerCase().includes('message:')) {
-                room = msg.match(/(?<=room:)[\s\S]*(?=message:)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
-            } else {
-                room = msg.match(/(?<=room:)[\s\S]*(?=\n)/gmi)[0].replace(/\n/gmi, '').replace(/\r/gmi, ' ').trim()
-            }
-            var extraInfo = msg.substring((msg.indexOf(room) + room.length))
+
         }
+        // todo: else if, make request to tourninfo. get event and call messageProcessing with specific event
         var searchStr0Res = await collection.find({
             trackedTeamCode: searchString[0],
             tournStart: {
@@ -77,33 +132,6 @@ router.post('/', upload.any(), async (req, resApp) => {
             console.log(`${searchString[1]} found in db`)
 
             notifyServer(searchStr1Res[0], { msg: msg || req.body.text, competition: competition, title: title, judging: judging, start: start, room: room, extraInfo: extraInfo, bye: (req.body.text.split('\n').length < 2 && req.body.text.includes('BYE')) })
-        }
-        async function notifyServer(searchStrRes, content) {
-            var notifyArr = searchStrRes.notify
-            for (const server of notifyArr) {
-                var sendGuild = await client.guilds.fetch(server.server)
-                if (sendGuild.available) {
-                    var guildChannels = await sendGuild.channels.fetch(server.channel)
-                    try {
-                        var tagging = server.users.map(user => `<@${user}>`).join(', ') + '\n' + (server.role !== null ? `<@&${server.role}>` : '')
-                        const embed = new Discord.MessageEmbed()
-                            .setTitle(`${content.title} Pairings`)
-                            .setColor('#daeaf1')
-                        if (content.bye) {
-                            embed.addField('Competition', content.competition)
-                        } else {
-                            embed.addField('Competition', content.competition)
-                            embed.addField('Judging', content.judging)
-                            embed.addField('Start Time', content.start)
-                            embed.addField('Room', content.room)
-                            embed.addField('Extra Info', content.extraInfo)
-                        }
-                        guildChannels.send({ content: tagging, embeds: [embed] })
-                    } catch (err) {
-                        console.error(err)
-                    }
-                }
-            }
         }
     }
 
