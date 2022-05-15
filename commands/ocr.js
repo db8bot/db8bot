8e3e0322ce330c21bf937ca3c70c56ec985c9dcc
Jim Fang <jfang.cv.ca.us@gmail.com>
2022-05-15 03:42:06 -0700

    ocr works, crashes on 2nd repeated use

--- commands/ocr.js
+++ commands/ocr.js
@@ -1,4 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const { createWorker } = require('tesseract.js')
+
+const worker = createWorker()
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,8 +14,19 @@ module.exports = {
         ),
     async execute(interaction) {
         require('../modules/telemetry').telemetry(__filename, interaction)
+        const image = interaction.options.getAttachment('image')
         console.log(interaction.options.getAttachment('image').contentType)
         console.log(interaction.options.getAttachment('image').proxyURL)
         console.log(interaction.options.getAttachment('image').url)
+
+        if (image.contentType.includes('image')) {
+            await interaction.reply('OCR Request has been added to the queue. You should see a message in this channel with the OCRed content shortly.')
+            await worker.load()
+            await worker.loadLanguage('eng')
+            await worker.initialize('eng')
+            const { data: { text } } = await worker.recognize(image.url)
+            await interaction.channel.send('```' + text.trim() + '```')
+            await worker.terminate()
+        }
     }
 }
