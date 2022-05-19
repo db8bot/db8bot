7364b7e83a685930777d67565ac216dd70fe8ce0
AirFusion45 <jfang.cv.ca.us@gmail.com>
2022-05-19 01:55:07 -0700

    add image as attachment option to /clean

--- commands/clean.js
+++ commands/clean.js
@@ -10,93 +10,101 @@ module.exports = {
             option.setName('link')
                 .setDescription('link to the image (ex: cdn.discordapp.com)')
                 .setRequired(false)
+        )
+        .addAttachmentOption(option =>
+            option.setName('image')
+                .setDescription('image to perform OCR on')
+                .setRequired(false)
         ),
     async execute(interaction) {
         require('../modules/telemetry').telemetry(__filename, interaction)
-        const args = interaction.options.getString('link')
-        interaction.channel.messages.fetch({ limit: 1 }).then(chanmsg => {
-            if (chanmsg.last().content.trim().match(/https:\/\/(cdn|media).(discordapp|discord).(com|net)\/attachments/g) && chanmsg.last().attachments.first() === undefined) { // no image in current msg, check for url pasted img in last msg
-                var imgURL = chanmsg.last().content.trim().match(/https:\/\/(cdn|media).(discordapp|discord).(com|net)\/attachments\/[0-9]{0,18}\/[0-9]{0,18}\/[@"^[\w\-. ]+.(png|jpeg|jpg|webp|gif)/gmi)[0]
-                superagent.get(imgURL).pipe(
-                    new PNG({
-                        colorType: 2,
-                        bgColor: {
-                            red: 255,
-                            green: 255,
-                            blue: 255
+        const args = interaction.options.getString('link') || (interaction.options.getAttachment('image') !== null ? interaction.options.getAttachment('image').url : null)
+        if (args != null && (args.trim().match(/https:\/\/(cdn|media).(discordapp|discord).(com|net)\/(attachments|(ephemeral-attachments))/g))) {
+            superagent.get(args).pipe(
+                new PNG({
+                    colorType: 2,
+                    bgColor: {
+                        red: 255,
+                        green: 255,
+                        blue: 255
+                    }
+                })
+            ).on('parsed', async function () {
+                var sendDataArr = []
+                const createWriteStream = () => {
+                    return stream.Writable({
+                        write(chunk, enc, next) {
+                            sendDataArr.push(chunk)
+                            next()
                         }
                     })
-                ).on('parsed', async function () {
-                    var sendDataArr = []
-                    const createWriteStream = () => {
-                        return stream.Writable({
-                            write(chunk, enc, next) {
-                                sendDataArr.push(chunk)
-                                next()
+                }
+                const writeStream = createWriteStream()
+                this.pack().pipe(writeStream)
+                writeStream.on('finish', () => {
+                    interaction.reply({ files: [Buffer.concat(sendDataArr)] })
+                })
+            })
+        } else {
+            interaction.channel.messages.fetch({ limit: 1 }).then(chanmsg => {
+                if (chanmsg.last().content.trim().match(/https:\/\/(cdn|media).(discordapp|discord).(com|net)\/attachments/g) && chanmsg.last().attachments.first() === undefined) { // no image in current msg, check for url pasted img in last msg
+                    var imgURL = chanmsg.last().content.trim().match(/https:\/\/(cdn|media).(discordapp|discord).(com|net)\/attachments\/[0-9]{0,18}\/[0-9]{0,18}\/[@"^[\w\-. ]+.(png|jpeg|jpg|webp|gif)/gmi)[0]
+                    superagent.get(imgURL).pipe(
+                        new PNG({
+                            colorType: 2,
+                            bgColor: {
+                                red: 255,
+                                green: 255,
+                                blue: 255
                             }
                         })
-                    }
-                    const writeStream = createWriteStream()
-                    this.pack().pipe(writeStream)
-                    writeStream.on('finish', () => {
-                        interaction.reply({ files: [Buffer.concat(sendDataArr)] })
-                    })
-                })
-            } else if (chanmsg.first().attachments.first() !== undefined) {
-                superagent.get(chanmsg.first().attachments.first().url).pipe(
-                    new PNG({
-                        colorType: 2,
-                        bgColor: {
-                            red: 255,
-                            green: 255,
-                            blue: 255
+                    ).on('parsed', async function () {
+                        var sendDataArr = []
+                        const createWriteStream = () => {
+                            return stream.Writable({
+                                write(chunk, enc, next) {
+                                    sendDataArr.push(chunk)
+                                    next()
+                                }
+                            })
                         }
+                        const writeStream = createWriteStream()
+                        this.pack().pipe(writeStream)
+                        writeStream.on('finish', () => {
+                            interaction.reply({ files: [Buffer.concat(sendDataArr)] })
+                        })
                     })
-                ).on('parsed', async function () {
-                    var sendDataArr = []
-                    const createWriteStream = () => {
-                        return stream.Writable({
-                            write(chunk, enc, next) {
-                                sendDataArr.push(chunk)
-                                next()
+                } else if (chanmsg.first().attachments.first() !== undefined) {
+                    superagent.get(chanmsg.first().attachments.first().url).pipe(
+                        new PNG({
+                            colorType: 2,
+                            bgColor: {
+                                red: 255,
+                                green: 255,
+                                blue: 255
                             }
                         })
-                    }
-                    const writeStream = createWriteStream()
-                    this.pack().pipe(writeStream)
-                    writeStream.on('finish', () => {
-                        interaction.reply({ files: [Buffer.concat(sendDataArr)] })
-                    })
-                })
-            } else if (args != null && (args.trim().match(/https:\/\/(cdn|media).(discordapp|discord).(com|net)\/attachments/g))) {
-                superagent.get(args).pipe(
-                    new PNG({
-                        colorType: 2,
-                        bgColor: {
-                            red: 255,
-                            green: 255,
-                            blue: 255
+                    ).on('parsed', async function () {
+                        var sendDataArr = []
+                        const createWriteStream = () => {
+                            return stream.Writable({
+                                write(chunk, enc, next) {
+                                    sendDataArr.push(chunk)
+                                    next()
+                                }
+                            })
                         }
-                    })
-                ).on('parsed', async function () {
-                    var sendDataArr = []
-                    const createWriteStream = () => {
-                        return stream.Writable({
-                            write(chunk, enc, next) {
-                                sendDataArr.push(chunk)
-                                next()
-                            }
+                        const writeStream = createWriteStream()
+                        this.pack().pipe(writeStream)
+                        writeStream.on('finish', () => {
+                            interaction.reply({ files: [Buffer.concat(sendDataArr)] })
                         })
-                    }
-                    const writeStream = createWriteStream()
-                    this.pack().pipe(writeStream)
-                    writeStream.on('finish', () => {
-                        interaction.reply({ files: [Buffer.concat(sendDataArr)] })
                     })
-                })
-            } else {
-                interaction.reply('Please specify a link or make sure the message above the command message is an image')
-            }
-        })
+                }
+                else {
+                    interaction.reply('Please specify a link or make sure the message above the command message is an image')
+                }
+            })
+        }
     }
 }
