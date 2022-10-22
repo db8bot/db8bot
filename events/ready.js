95eb6b880175b4367a5af314f5ea4df729821dd7
AirFusion45 <jfang.cv.ca.us@gmail.com>
2022-10-21 18:13:02 -0700

    starting rewrite

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,16 @@
-module.exports = (client) => {
-    const chalk = require('chalk')
-    console.log(chalk.green('|--------------------(Loading Complete)------------------------|'))
-    console.log(chalk.green('db8bot is now online and ready to go! Here are some information:'))
-    console.log(chalk.green(`db8bot loaded successfully @ ${Date()}`))
-    console.log(chalk.green(`Owner: ${process.env.OWNERTAG}`))
-    console.log(chalk.green(`Logged in as: ${process.env.NAME} `))
-    console.log(chalk.green(`Prefix: ${process.env.PREFIX}`))
-    client.user.setActivity(`/help | db8bot | Version: ${require('../package.json').version} ${process.env.BUILD}`)
-    // client.application.commands.set([]) // reset client application slash commands
+const { Events } = require('discord.js')
+const chalk = require('chalk')
+
+module.exports = {
+    name: Events.ClientReady,
+    once: true,
+    execute(client) {
+        console.log(chalk.green('|--------------------(Loading Complete)------------------------|'))
+        console.log(chalk.green('db8bot is now online and ready to go! Here are some information:'))
+        console.log(chalk.green(`db8bot loaded successfully @ ${Date()}`))
+        console.log(chalk.green(`Owner: ${process.env.OWNERTAG}`))
+        console.log(chalk.green(`Logged in as: ${process.env.NAME} `))
+        client.user.setActivity(`/help | db8bot v${require('../package.json').version} ${process.env.BUILD}`)
+        // client.application.commands.set([]) // reset client application slash commands
+    }
 }
